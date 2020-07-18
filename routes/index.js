2c1b0ca9755eb9e310a63b9432dfe7852b427f56
Mubashir Hanif <mubashirhanif@users.noreply.github.com>
2020-07-18 21:26:57 +0200

    Added Tag module (#11)

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const userRoutes = require("./user.route");
 const serviceRoutes = require("./service.route");
+const tagRoutes = require("./tag.route");
 const reviewRoutes = require("./review.route");
 
 /**
@@ -11,6 +12,7 @@ const init = (app) => {
   // add routes
   app.use("/api/user", userRoutes);
   app.use("/api/service", serviceRoutes);
+  app.use("/api/tag", tagRoutes);
   app.use("/api/review", reviewRoutes);
 };
 
