630b653b3a00553781d6fca921483c3b754d364e
Mubashir Hanif <mubashirhanif93@gmail.com>
2020-07-18 18:51:36 +0200

    Added initial routes for services

--- src/service/service.actions.js
+++ src/service/service.actions.js
@@ -1,27 +1,65 @@
 "use strict";
 
-const serviceModel = require("./service.model");
-
+const Service = require("./service.model");
+const { logger } = require("../config");
 const actions = {};
 
 //GET method search services by keyword or tags.
 actions.searchServices = (req, res, next) => {
-  // req.keywords, req.tags
+  // TODO: use req.keywords, req.tags
+  Service.find((error, data) => {
+    if (error) {
+      logger.debug(`[Search Service] Failed with error: ${error.message}`);
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
 };
 
 //Create service using all the parameters
 actions.createService = (req, res, next) => {
-  //
+  // req.body has all the parameters.
+  Service.create(req.body, (error, data) => {
+    if (error) {
+      logger.debug(`[Create Service] Failed with error: ${error.message}`);
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
 };
 
 //Update service using all the parameters
+// req.param.id
 actions.updateService = (req, res, next) => {
-  //
+  Service.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        logger.debug(`[Update Service] Failed with error: ${error.message}`);
+        return next(error);
+      } else {
+        res.json(data);
+      }
+    }
+  );
 };
 
 // Delete service
 actions.deleteService = (req, res, next) => {
   //
+  Service.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      logger.debug(`[Delete Service] Failed with error: ${error.message}`);
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
 };
 
 module.exports = actions;
