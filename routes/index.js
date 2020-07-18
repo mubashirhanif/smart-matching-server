afdd690cac089e41e8b8be6b033c91d359d6c7d7
Mubashir Hanif <mubashirhanif@users.noreply.github.com>
2020-07-18 21:28:02 +0200

    Added Transaction module (#12)

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const userRoutes = require("./user.route");
 const serviceRoutes = require("./service.route");
+const transactionRoutes = require("./transaction.route");
 const tagRoutes = require("./tag.route");
 const reviewRoutes = require("./review.route");
 
@@ -12,6 +13,7 @@ const init = (app) => {
   // add routes
   app.use("/api/user", userRoutes);
   app.use("/api/service", serviceRoutes);
+  app.use("/api/transaction", transactionRoutes);
   app.use("/api/tag", tagRoutes);
   app.use("/api/review", reviewRoutes);
 };
