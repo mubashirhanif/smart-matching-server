e3911eb189cfd2ee2f069861d650dab12dad6de7
Mubashir Hanif <mubashirhanif93@gmail.com>
2020-07-18 18:26:09 +0200

    Added dummy actions and routes

--- src/service/service.actions.js
+++ src/service/service.actions.js
@@ -1,5 +1,27 @@
 "use strict";
 
+const serviceModel = require("./service.model");
+
 const actions = {};
+
+//GET method search services by keyword or tags.
+actions.searchServices = (req, res, next) => {
+  // req.keywords, req.tags
+};
+
+//Create service using all the parameters
+actions.createService = (req, res, next) => {
+  //
+};
+
+//Update service using all the parameters
+actions.updateService = (req, res, next) => {
+  //
+};
+
+// Delete service
+actions.deleteService = (req, res, next) => {
+  //
+};
 
 module.exports = actions;
